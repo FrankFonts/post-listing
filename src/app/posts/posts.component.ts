67d3b08d9796fe8e98505061bdca85cf0c2aaa31
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2022-11-01 15:33:15 +0100

    Simple fetch works

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,5 +11,9 @@ export class PostsComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
 }
